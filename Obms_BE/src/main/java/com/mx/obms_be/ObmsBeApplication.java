2560c13064d9384cb0b0e560a4201d5433b60ed4
Flandre3569 <1332726596@qq.com>
2022-06-12 00:15:13 +0800

    [init] back-end

---
+++ Obms_BE/src/main/java/com/mx/obms_be/ObmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.mx.obms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObmsBeApplication.class, args);
+    }
+
+}
---
+++ Obms_BE/src/test/java/com/mx/obms_be/ObmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.obms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
